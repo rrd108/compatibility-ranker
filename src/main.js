cf1c613a8c0fa5de2a9e1c051edf15102e3e4f03
rrd <rrd@1108.cc>
2021-02-13 12:35:20 +0100

    new icons added

--- src/main.js
+++ src/main.js
@@ -2,9 +2,9 @@ import Vue from 'vue'
 import App from './App.vue'
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
-import { faBaby, faBomb, faClock, faGlobe, faHeart, faKey, faLink, faMarsStroke, faMeteor, faMoon, faUserAstronaut, faUserPlus } from '@fortawesome/free-solid-svg-icons'
+import { faBaby, faBomb, faClock, faGlobe, faHeart, faInfo, faKey, faLink, faMarsStroke, faMeteor, faMoon, faSave, faUserAstronaut, faUserPlus } from '@fortawesome/free-solid-svg-icons'
 
-library.add(faBaby, faBomb, faClock, faGlobe, faHeart, faKey, faLink, faMarsStroke, faMeteor, faMoon, faUserAstronaut, faUserPlus)
+library.add(faBaby, faBomb, faClock, faGlobe, faHeart, faInfo, faKey, faLink, faMarsStroke, faMeteor, faMoon, faSave, faUserAstronaut, faUserPlus)
 
 Vue.component('font-awesome-icon', FontAwesomeIcon)
 
