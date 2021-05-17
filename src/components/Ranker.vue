ec1273ae7d5368a94ee6e3893b632f07ae570042
rrd <rrd@1108.cc>
2021-05-17 15:06:46 +0200

    prettier fix

--- src/components/Ranker.vue
+++ src/components/Ranker.vue
@@ -2,21 +2,44 @@
   <div>
     <div class="row">
       <h3>Name</h3>
-      <router-link to="addPerson"><font-awesome-icon icon="user-plus" /></router-link>
+      <router-link to="addPerson"
+        ><font-awesome-icon icon="user-plus"
+      /></router-link>
     </div>
     <select @change="getAnalysis" v-model="personId">
-      <option v-for="person in people" :key="person.id" :value="person.id">{{person.name}} ({{person.birth_date.substring(0,4)}})</option>
+      <option v-for="person in people" :key="person.id" :value="person.id">
+        {{ person.name }} ({{ person.birth_date.substring(0, 4) }})
+      </option>
     </select>
 
     <article v-show="targetPerson">
-      <h1>{{targetPerson ? targetPerson.name : ''}}</h1>
-      <h2>{{targetPerson ? `${targetPerson.birth_date}, ${targetPerson.birth_time}, ${targetPerson.birth_place}` : ''}}</h2>
+      <h1>{{ targetPerson ? targetPerson.name : "" }}</h1>
+      <h2>
+        {{
+          targetPerson
+            ? `${targetPerson.birth_date}, ${targetPerson.birth_time}, ${targetPerson.birth_place}`
+            : ""
+        }}
+      </h2>
       <font-awesome-icon icon="sync" spin v-show="loading" />
-      <h3 class="pointer" v-show="!loading" @click="moonData(targetPerson)" title="Get Moon Data">
-        <font-awesome-icon icon="moon" /> {{targetPerson ? zodiacs[targetPerson.moon] : ''}} {{targetPerson ? targetPerson.moon : ''}}
+      <h3
+        class="pointer"
+        v-show="!loading"
+        @click="moonData(targetPerson)"
+        title="Get Moon Data"
+      >
+        <font-awesome-icon icon="moon" />
+        {{ targetPerson ? zodiacs[targetPerson.moon] : "" }}
+        {{ targetPerson ? targetPerson.moon : "" }}
       </h3>
-      <h3 class="pointer" v-show="!loading" @click="moonData(targetPerson)" title="Get Moon Data">
-        <font-awesome-icon icon="meteor" /> {{targetPerson ? targetPerson.naksatra : ''}}
+      <h3
+        class="pointer"
+        v-show="!loading"
+        @click="moonData(targetPerson)"
+        title="Get Moon Data"
+      >
+        <font-awesome-icon icon="meteor" />
+        {{ targetPerson ? targetPerson.naksatra : "" }}
       </h3>
       <div class="info">
         <font-awesome-icon icon="info" />
@@ -27,15 +50,33 @@
 
     <div>
       <section v-for="partner in possiblePartners" :key="partner.id">
-        <h2>{{partner.name}}  <font-awesome-icon icon="link" @click="personId = partner.id;getAnalysis()" /></h2>
-        <h3>{{partner.birth_date}} {{partner.birth_time}}, {{partner.birth_place}}</h3>
-        <h4><font-awesome-icon icon="moon" /> {{zodiacs[partner.moon]}} {{partner.moon}}</h4>
-        <h4><font-awesome-icon icon="meteor" /> {{partner.naksatra}}</h4>
+        <h2>
+          {{ partner.name }}
+          <font-awesome-icon
+            icon="link"
+            @click="
+              personId = partner.id
+              getAnalysis()
+            "
+          />
+        </h2>
+        <h3>
+          {{ partner.birth_date }} {{ partner.birth_time }},
+          {{ partner.birth_place }}
+        </h3>
+        <h4>
+          <font-awesome-icon icon="moon" /> {{ zodiacs[partner.moon] }}
+          {{ partner.moon }}
+        </h4>
+        <h4><font-awesome-icon icon="meteor" /> {{ partner.naksatra }}</h4>
         <ul>
-          <li><span>{{parseInt(partner.points / maxPoints * 100)}}%</span> ({{partner.points}} points)</li>
-          <li :class="{outRange : outMoonRange(partner.stridirgha)}"><span>{{zodiacs[partner.moon]}}</span>{{partner.moon}}<br>Stridirgha: {{partner.stridirgha}}</li>
-          <li><span>{{partner.age_difference}}</span> év korkülönbség</li>
-        </ul>
+          <li>
+            <span>{{ parseInt((partner.points / maxPoints) * 100) }}%</span> ({{
+              partner.points
+            }}
+            points)
+          </li>
+          <li>
             <ul id="additional">
               <li :class="{ outRange: outMoonRange(partner.stridirgha) }">
                <h5>Stridirgha: {{ partner.stridirgha }}</h5>
@@ -52,19 +93,24 @@
                <h5>Raja</h5>
               </li>
             </ul>
-        <Stars :points="partner.points" />
+          </li>
+          <li>
+            <span>{{ partner.age_difference }}</span> év korkülönbség
+          </li>
+        </ul>
+        <Stars :points="partner.points" /><!-- TODO here conside points + 4 additional -->
       </section>
     </div>
   </div>
 </template>
 
 <script>
-import axios from 'axios'
-import Stars from '@/components/Stars'
+import axios from "axios"
+import Stars from "@/components/Stars"
 export default {
-  name: 'Ranker',
-  components: {Stars},
-  props: ['token'],
+  name: "Ranker",
+  components: { Stars },
+  props: ["token"],
   data() {
     return {
       loading: false,
@@ -74,75 +120,106 @@ export default {
       personId: null,
       showAdd: false,
       showEditInfo: false,
-      zodiacs: {'Aries': '♈', 'Taurus': '♉', 'Gemini' : '♊', 'Cancer' : '♋'	, 'Leo' : '♌', 'Virgo' : '♍', 'Libra' : '♎', 'Scorpio': '♏', 'Sagittarius' : '♐', 'Capricorn': '♑', 'Aquarius': '♒', 'Pisces' : '♓'}
+      zodiacs: {
+        Aries: "♈",
+        Taurus: "♉",
+        Gemini: "♊",
+        Cancer: "♋",
+        Leo: "♌",
+        Virgo: "♍",
+        Libra: "♎",
+        Scorpio: "♏",
+        Sagittarius: "♐",
+        Capricorn: "♑",
+        Aquarius: "♒",
+        Pisces: "♓",
+      },
     }
   },
 
   created() {
-    axios.get(`${process.env.VUE_APP_API_URL}?names`,
-      {headers: {Authorization: `ApiKey ${this.token}`}})
-      .then(response => this.people = response.data)
-      .catch(error => console.error(error))
+    axios
+      .get(`${process.env.VUE_APP_API_URL}?names`, {
+        headers: { Authorization: `ApiKey ${this.token}` },
+      })
+      .then((response) => (this.people = response.data))
+      .catch((error) => console.error(error))
   },
 
   computed: {
     targetPerson() {
-      return this.people.find(person => person.id == this.personId)
+      return this.people.find((person) => person.id == this.personId)
     },
     targetPersonInfo: {
       get() {
-        return this.targetPerson ? this.targetPerson.info : ''
+        return this.targetPerson ? this.targetPerson.info : ""
       },
       set(value) {
         this.targetPerson.info = value
-      }
-    }
+      },
+    },
   },
 
   methods: {
     getAnalysis() {
-      axios.get(`${process.env.VUE_APP_API_URL}?analysis=${this.personId}`,
-        {headers: {Authorization: `ApiKey ${this.token}`}})
-        .then(response => this.possiblePartners = response.data.sort((a, b) => b.points - a.points))
-        .catch(error => console.error(error))
+      axios
+        .get(`${process.env.VUE_APP_API_URL}?analysis=${this.personId}`, {
+          headers: { Authorization: `ApiKey ${this.token}` },
+        })
+        .then(
+          (response) =>
+            (this.possiblePartners = response.data.sort(
+              (a, b) => b.points - a.points
+            ))
+        )
+        .catch((error) => console.error(error))
     },
     moonData(person) {
       this.loading = true
-      axios.get(`${process.env.VUE_APP_API_URL}?moonData=${person.id}`,
-        {headers: {Authorization: `ApiKey ${this.token}`}})
-        .then(response => {
+      axios
+        .get(`${process.env.VUE_APP_API_URL}?moonData=${person.id}`, {
+          headers: { Authorization: `ApiKey ${this.token}` },
+        })
+        .then((response) => {
           this.loading = false
-          if (person.naksatra != response.data.naksatra || person.moon != response.data.moon) {
+          if (
+            person.naksatra != response.data.naksatra ||
+            person.moon != response.data.moon
+          ) {
             person.naksatra = response.data.naksatra
             person.moon = response.data.moon
-            axios.patch(
-              process.env.VUE_APP_API_URL,
-              {
-                id: person.id,
-                naksatra: person.naksatra,
-                moon: person.moon
-              },
-              { headers: {Authorization: `ApiKey ${this.token}`} })
-              .then(response => console.log(response.data))
-            }
-          })
-        .catch(error => console.error(error))
+            axios
+              .patch(
+                process.env.VUE_APP_API_URL,
+                {
+                  id: person.id,
+                  naksatra: person.naksatra,
+                  moon: person.moon,
+                },
+                { headers: { Authorization: `ApiKey ${this.token}` } }
+              )
+              .then((response) => console.log(response.data))
+          }
+        })
+        .catch((error) => console.error(error))
     },
     outMoonRange(points) {
       return points > 6
     },
     saveInfo() {
-      axios.patch(
-        process.env.VUE_APP_API_URL,
-        {
-          id: this.targetPerson.id,
-          info: this.targetPerson.info
-        },
-        { headers: {Authorization: `ApiKey ${this.token}`} })
-        .then(response => console.log(response.data))
-        .catch(error => console.error(error))
+      axios
+        .patch(
+          process.env.VUE_APP_API_URL,
+          {
+            id: this.targetPerson.id,
+            info: this.targetPerson.info,
+          },
+          { headers: { Authorization: `ApiKey ${this.token}` } }
+        )
+        .then((response) => console.log(response.data))
+        .catch((error) => console.error(error))
     },
-  }
+  },
 }
 </script>
 
@@ -214,6 +291,7 @@ span {
   font-size: 15vw;
   display: block;
 }
+h5 {font-size: 1.1rem;}
 .outRange {
   background-color: #d64933;
 }
@@ -232,6 +310,21 @@ span {
   span {
     font-size: 10vw;
   }
-}
 
+  #additional {
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    grid-template-rows: 1fr 1fr;
+    grid-gap: .3em;
+    margin: 0;
+  }
+  #additional li {
+    width: auto;
+    height: auto;
+    margin: 0;
+  }
+  #additional span {
+    font-size: 2rem;
+  }
+}
 </style>
