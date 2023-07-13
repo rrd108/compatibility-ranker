5155cbd307e19dded99755d0aadad9776b068acf
rrd108 <rrd@webmania.cc>
2023-07-13 20:12:40 +0200

    build option added

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,17 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import path from 'path'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'), // map '@' to './src'
+export default defineConfig(({ command, mode }) => {
+  const env = loadEnv(mode, process.cwd(), 'VITE_')
+  return {
+    plugins: [vue()],
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, './src'), // map '@' to './src'
+      },
     },
-  },
+    base: env.VITE_APP_BASE_DIR,
+  }
 })
