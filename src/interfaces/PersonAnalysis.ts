0c7f9bde1e036e575dd91f8fed07defd784930fd
rrd108 <rrd@webmania.cc>
2023-07-15 12:44:57 +0200

    new property added

--- src/interfaces/PersonAnalysis.ts
+++ src/interfaces/PersonAnalysis.ts
@@ -1,6 +1,7 @@
 import PersonMoonData from './PersonMoonData'
 
 export default interface Analysis extends PersonMoonData {
+  age_difference: number
   points: number
   stridirgha: number
   rashi: number
